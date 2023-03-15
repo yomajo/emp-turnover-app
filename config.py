ae9b5875398d384bb1f41891184c9d9fcd8a0109
yomajo <ed.vinas@yahoo.com>
2023-03-15 16:54:04 +0200

    add flag to control what to do w/ csv old files

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config():
     SQLALCHEMY_DATABASE_URI = 'sqlite:///companies.db'
     STATS_SINCE = '2020-01'     # Min available '2018-01'
     DELIMITER = ';'
+    DELETE_CSV_FILES = False
     COMPANIES_OF_INTEREST = [
         'devbridge',
         'maxima',
